2c25f09699c8cdf054e0a44146fc54e2d08dc1f7
mininao <maxence_aici@yahoo.fr>
2015-07-09 23:31:13 +0200

    Cleanup & update rooms

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,26 +3,7 @@ angular.module('bdeesiee.controllers', [])
 .controller('DashCtrl', function ($scope) {})
 
 .controller('RoomsCtrl', function ($scope, Rooms, $ionicPopup) {
-	var roomCategory = function(room) {
-		if(isNaN(room))
-			return 0;
-		var roomNumber = parseInt(room);
-		if(roomNumber > 999 && roomNumber < 8000) {
-			return Math.ceil(roomNumber/1000)
-		} else {
-			return 0;
-		}
-	}
-	$scope.zones = [
-		{name:"Autres",rooms:[]},	
-		{name:"Épi 1",rooms:[]},
-		{name:"Épi 2",rooms:[]},
-		{name:"Épi 3",rooms:[]},
-		{name:"Épi 4",rooms:[]},
-		{name:"Épi 5",rooms:[]},
-		{name:"Épi 6",rooms:[]},
-		{name:"Épi 7",rooms:[]},
-	]
+
 	$scope.doRefresh = function(){
 		Rooms.all().then(function (data) {
 			console.log(data);
--- www/js/services.js
+++ www/js/services.js
@@ -1,16 +1,37 @@
 angular.module('bdeesiee.services', [])
 
 .factory('Rooms', ['$http', '$log', function ($http, $log) {
-
-	return {
-		all: function () {
-			var roomsPromise = $http.get('http://localhost:8080/api/rooms').
-			then(function (data, status, headers, config) {
-				return {
-					labello: "cactus"
-				};
-			});
-			return roomsPromise;
+	var roomCategory = function (room) {
+		if (isNaN(room))
+			return 0;
+		var roomNumber = parseInt(room);
+		if (roomNumber > 999 && roomNumber < 8000) {
+			return Math.ceil(roomNumber / 1000)
+		} else {
+			return 0;
 		}
+	};
+	var zones = [
+		{name:"Autres",rooms:[]},	
+		{name:"Épi 1",rooms:[]},
+		{name:"Épi 2",rooms:[]},
+		{name:"Épi 3",rooms:[]},
+		{name:"Épi 4",rooms:[]},
+		{name:"Épi 5",rooms:[]},
+		{name:"Épi 6",rooms:[]},
+		{name:"Épi 7",rooms:[]},
+	];
+	var getZones = function () {
+		var zonesPromise = $http.get('http://localhost:8080/api/rooms').
+		then(function (data, status, headers, config) {
+			
+			return {
+				labello: "cactus"
+			};
+		});
+		return zonesPromise;
+	}
+	return {
+		zones: getZones
 	};
 }]);
