271bf770ae2d9664d36c43264dd150289cf0de2d
mininao <maxence_aici@yahoo.fr>
2017-01-07 16:17:01 +0100

    Update event card date format

--- components/events/EventCard.js
+++ components/events/EventCard.js
@@ -3,12 +3,25 @@ import {
   Text,
   View
 } from 'react-native';
+import moment from 'moment';
+import 'moment/locale/fr';
 
 import styles from '../styles/events.js'
 import { Button, Card } from 'react-native-material-design';
 
 class EventCard extends Component {
   render() {
+    let start = moment(this.props.event.start);
+    let end = moment(this.props.event.end);
+    let eventDate = '';
+    moment.locale('fr');
+
+    if (start.isSame(end, 'day')) {
+        eventDate = start.format('[Le] D MMMM [de] HH[h]mm') + end.format(' [à] HH[h]mm');
+    } else {
+        eventDate = start.format('[Du] D MMMM HH[h]mm') + end.format(' [au] D MMMM HH[h]mm');
+    }
+
     return (
       <View >
           <Card>
@@ -18,9 +31,7 @@ class EventCard extends Component {
               />*/}
               <Card.Body>
                   <Text style={{fontWeight: "bold", color:"#000"}}>{this.props.event.title}</Text>
-                    <Text>Début : {this.props.event.start}</Text>
-                    <Text>Fin : {this.props.event.end}</Text>
-
+                  <Text>{eventDate}</Text>
               </Card.Body>
               <Card.Actions position="right">
                   {/*<Button text="VOIR" onPress={this.openLink.bind(this,item.link)} overrides={{textColor: "#000"}} />*/}
