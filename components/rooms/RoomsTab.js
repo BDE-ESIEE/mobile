9909595c55632ec0ab48bf4e285e0989486295e6
mininao <maxence_aici@yahoo.fr>
2017-03-13 05:05:24 +0100

    Ameliorations UI rooms Changed icons, disabled button goind back in time

--- components/rooms/RoomsTab.js
+++ components/rooms/RoomsTab.js
@@ -6,10 +6,14 @@ import {
   StatusBar,
   TouchableOpacity
 } from 'react-native';
+
 import LinearGradient from 'react-native-linear-gradient';
 import moment from 'moment';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import styles from '../styles/rooms.js';
 import RoomsList from './RoomsList';
+
 moment.locale('fr');
 
 class RoomsTab extends Component {
@@ -33,19 +37,25 @@ class RoomsTab extends Component {
             start={{x: 0.0, y: 0}} end={{x: 1, y: 1}}
             colors={['#FE734C', '#FF4D59']}
             style={styles.topBar}>
-            <TouchableOpacity onPress={()=>this.decrTime()}>
-              <Text style={styles.topBarButton}>
-                -
-              </Text>
+            <TouchableOpacity
+              onPress={()=>this.decrTime()}
+              activeOpacity={this.state.timeDiff ? 0.2:1}>
+              <Icon
+                name='ios-arrow-dropleft-outline'
+                style={[styles.topBarButton, {
+                  opacity: this.state.timeDiff ? 1:0.3
+                }]}
+              />
             </TouchableOpacity>
             <Text style={styles.topBarText}>
               Salles libres
               <Text style={{fontFamily:"ProximaNova-RegItalic"}}> {timeName}</Text>
             </Text>
             <TouchableOpacity onPress={()=>this.incrTime()}>
-              <Text style={styles.topBarButton}>
-                +
-              </Text>
+              <Icon
+                name='ios-arrow-dropright-outline'
+                style={styles.topBarButton}
+              />
             </TouchableOpacity>
           </LinearGradient>
         </View>
