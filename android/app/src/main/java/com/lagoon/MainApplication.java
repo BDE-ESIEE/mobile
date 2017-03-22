97dcccb187de41c20de87aceebc42f1f95b23c98
mininao <maxence_aici@yahoo.fr>
2017-03-22 12:12:04 +0100

    Fix android typo

--- android/app/src/main/java/com/lagoon/MainApplication.java
+++ android/app/src/main/java/com/lagoon/MainApplication.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;s
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
