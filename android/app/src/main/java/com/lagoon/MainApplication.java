4eb6ffddc18e7ed560454a546bce8745e1d9338a
mininao <maxence_aici@yahoo.fr>
2017-03-22 14:48:20 +0100

    Adding native calendar events module #16

--- android/app/src/main/java/com/lagoon/MainApplication.java
+++ android/app/src/main/java/com/lagoon/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.calendarevents.CalendarEventsPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new CalendarEventsPackage(),
         new RNGoogleSigninPackage(),
         new VectorIconsPackage(),
         new LinearGradientPackage(),
