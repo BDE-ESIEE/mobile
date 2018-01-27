8ea4e01d787234b5d45eee4ce8dd71808804595d
Kévin Ta <ta_kevin@hotmail.fr>
2018-01-28 01:03:26 +0800

    Added FairPay features on login tab

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  StatusBar,
+  TouchableOpacity
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { GoogleSigninButton } from 'react-native-google-signin';
 import { Button } from 'react-native-material-design';
+import Barcode from 'react-native-barcode-builder';
 
 import Auth from '../libs/auth';
 import styles from './styles/login.js';
@@ -16,7 +20,9 @@ class LoginPage extends Component {
     this.state = {
       isLoggedIn: false,
       authCallbackIndex: null,
-      authError: null
+      authError: null,
+      codeCantine: "",
+      fairPay: ""
     };
   }
 
@@ -41,12 +47,69 @@ class LoginPage extends Component {
     Auth.removeCallback('auth', this.state.index);
   }
 
+  getCodeCantine(email) {
+    fetch('https://bde.esiee.fr/fairpay/api/students/'+email+'/search', {
+      method: 'get'
+    })
+    .then((response) => {
+      response.json().then((json) => {
+        this.setState({
+          codeCantine: json[0].id,
+        });
+      })
+    });
+  }
+
+  getFairPayBalance(email) {
+    fetch('https://bde.esiee.fr/fairpay/api/students/'+email+'/search', {
+      method: 'get'
+    })
+    .then((response) => {
+      response.json()
+      .then((json) => {
+        fetch('https://bde.esiee.fr/fairpay/api/student/balance?client_id='+json[0].id, {
+          method: 'get'
+        })
+        .then((response) => {
+          response.json().then((json2) => {
+            this.setState({
+              codeCantine: json[0].id,
+              fairPay: json2.balance,
+            });
+          })
+        });
+      })
+    });
+  }
+
   render () {
     let text = 'Mon compte';
     let button;
+    let textFairpay = '';
+    let barcode;
 
     if (this.state.isLoggedIn) {
       text = Auth.getUser().name;
+      let codeCantine = '' + this.state.codeCantine;
+      let height = 50;
+      let width = 1.5;
+      this.getFairPayBalance(Auth.getUser().email);
+      if(this.state.fairPay) {
+        textFairpay = '\nTon solde FairPay : '+this.state.fairPay+'€';
+        barcode = (
+          <View style={styles.barcode}>
+            <Barcode 
+              value={codeCantine}
+              format="CODE39"
+              height={height}
+              width={width}
+            />
+            <Text style={styles.barcodeText}>
+              {codeCantine}
+            </Text>
+          </View>
+        );
+      }
       button = (
         // Button logout sa mère
         <Button
@@ -66,14 +129,33 @@ class LoginPage extends Component {
     }
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {text}
-        </Text>
-        <Text style={styles.welcome}>
-          {this.state.authError ? this.state.authError : ''}
-        </Text>
-        {button}
+      <View style={styles.containerBar}>
+        <StatusBar translucent={true} backgroundColor="rgba(0,0,0,0.2)" barStyle="light-content"/>
+        <View>
+          <LinearGradient
+            start={{x: 0.0, y: 0}} end={{x: 1, y: 1}}
+            colors={['#1D976C', '#1D976C']}
+            style={styles.topBar}>
+            <TouchableOpacity></TouchableOpacity>
+            <Text style={styles.topBarText}>
+              <Text style={styles.topBarNormalText}>Compte</Text>
+            </Text>
+            <TouchableOpacity></TouchableOpacity>
+          </LinearGradient>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            {text}
+          </Text>
+          <Text style={styles.fairpay}>
+            {textFairpay}
+          </Text>
+          {barcode}
+          <Text style={styles.welcome}>
+            {this.state.authError ? this.state.authError : ''}
+          </Text>
+          {button}
+        </View>
       </View>
     );
   }
--- components/styles/login.js
+++ components/styles/login.js
@@ -8,16 +8,64 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  containerBar: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
   welcome: {
     fontSize: 30,
     textAlign: 'center',
     margin: 10,
     fontFamily:"ProximaNova-Light"
   },
+  fairpay: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontFamily:"ProximaNova-Light"
+  },
+  barcode: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  barcodeText: {
+    fontSize: 15,
+    textAlign: 'center',
+    fontFamily:"ProximaNova-Light"
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5
+  },
+  topBar: {
+    paddingTop:25,
+    height:80,
+    flexDirection: "row",
+    justifyContent:"space-between",
+    alignItems:"center",
+  },
+  topBarText:{
+    backgroundColor:"transparent"
+  },
+  topBarNormalText: {
+    color:"#ffffff",
+    fontSize:16,
+    fontFamily:"ProximaNova-Bold"
+  },
+  topBarEmphasisText: {
+    color:"#ffffff",
+    fontSize:16,
+    fontFamily:"ProximaNova-BoldIt"
+  },
+  topBarButton: {
+    color:"#ffffff",
+    fontSize:25,
+    fontFamily:"ProximaNova-Light",
+    marginRight:20,
+    marginLeft:20,
+    backgroundColor:"transparent"
   }
 });
 
